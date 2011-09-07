0ad3b4444a4064629bd766bced61e2b80a88cd9f
Loren Yu <yuloren@gmail.com>
2011-09-06 23:45:45 -0700

    trivial commit

--- game/views.py
+++ game/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from game.models import Card
 
+
 def index(request):
     cards = Card.objects.all()
     return render_to_response('cards/index.html', {'cards' : cards})

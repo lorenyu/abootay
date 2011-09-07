3ae101b76507a95ea0b83b863b9fc8a6c3443f98
Arjun Agarwal <arjun.agarwal@stanfordalumni.org>
2011-09-06 23:24:31 -0700

    add to card model

--- game/models.py
+++ game/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
 class Card(models.Model):
-    phrase = models.CharField(max_length=256)
+    phrase = models.CharField(max_length=256,primary_key=True)
+    tabooWordOne = models.CharField(max_length=64)
+    tabooWordTwo = models.CharField(max_length=64)
+    tabooWordThree = models.CharField(max_length=64)
+    tabooWordFour = models.CharField(max_length=64)
+    difficultyLevel = models.IntegerField(null=True)
+
+    @property
+    def tabooWords( self ):
+        return[ tabooWordOne, tabooWordTwo, tabooWordThree, tabooWordFour ]

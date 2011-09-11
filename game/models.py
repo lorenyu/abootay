b4e57033daec64daaddb626c01982e27a21e90bb
Arjun Agarwal <arjun.agarwal@stanfordalumni.org>
2011-09-11 16:11:30 -0700

    push new database model including a games table

--- game/models.py
+++ game/models.py
@@ -1,13 +1,27 @@
 from django.db import models
 
 class Card(models.Model):
+    # Card will have a field called id, which is an auto-incrementing
+    # primary key
+    deck = models.ForeignKey('Deck', related_name = 'cards')
     phrase = models.CharField(max_length=256,primary_key=True)
     abootayWordOne = models.CharField(max_length=64)
     abootayWordTwo = models.CharField(max_length=64)
     abootayWordThree = models.CharField(max_length=64)
     abootayWordFour = models.CharField(max_length=64)
-    difficultyLevel = models.IntegerField(null=True)
+    abootayWordFive = models.CharField(max_length=64)
+    abootayWordSix = models.CharField(max_length=64)
 
     @property
     def tabooWords( self ):
-        return[ tabooWordOne, tabooWordTwo, tabooWordThree, tabooWordFour ]
+        return[ tabooWordOne, tabooWordTwo, tabooWordThree, tabooWordFour,
+                tabooWordFive, tabooWordSix ]
+
+class Deck( models.Model ):
+    # Deck will have a field called id, which is an auto-incrementing 
+    # primary key
+    # Deck will also have a field called 'cards' by virtue of the ForeignKey
+    # relationship with the Card class setup above
+    pass
+
+    

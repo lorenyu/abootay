1031b9588528badd1b664c9ffd5215860f384c16
Loren Yu <yuloren@gmail.com>
2011-09-11 20:52:17 -0700

    CharField cannot have max_length greater than 255 when using unique=True

--- game/models.py
+++ game/models.py
@@ -4,13 +4,13 @@ class Card(models.Model):
     # Card will have a field called id, which is an auto-incrementing
     # primary key
     deck = models.ForeignKey('Deck', related_name = 'cards')
-    phrase = models.CharField(max_length=256)
-    abootayWordOne = models.CharField(max_length=64)
-    abootayWordTwo = models.CharField(max_length=64)
-    abootayWordThree = models.CharField(max_length=64)
-    abootayWordFour = models.CharField(max_length=64)
-    abootayWordFive = models.CharField(max_length=64)
-    abootayWordSix = models.CharField(max_length=64)
+    phrase = models.CharField(max_length=255)
+    abootayWordOne = models.CharField(max_length=63)
+    abootayWordTwo = models.CharField(max_length=63)
+    abootayWordThree = models.CharField(max_length=63)
+    abootayWordFour = models.CharField(max_length=63)
+    abootayWordFive = models.CharField(max_length=63)
+    abootayWordSix = models.CharField(max_length=63)
 
     @property
     def tabooWords( self ):
@@ -22,5 +22,5 @@ class Deck( models.Model ):
     # primary key
     # Deck will also have a field called 'cards' by virtue of the ForeignKey
     # relationship with the Card class setup above
-    title = models.CharField( max_length=256, unique=True )
+    title = models.CharField( max_length=255, unique=True )
     

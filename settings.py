fad2dd48c223bc798bceefff8d5ebd856312ed1a
Loren Yu <yuloren@gmail.com>
2011-09-18 16:01:19 -0700

    Allow local configuration of template_dirs

--- settings.py
+++ settings.py
@@ -79,6 +79,7 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
+templates.configure(TEMPLATE_DIRS)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
